34b49a9ad0b3cdd8e8a7b070ff1fc3f7bc59c7e0
D4VGFX <d4v.gfx@gmail.com>
2025-05-19 13:40:59 +0200

    Velveta

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("Script Velveta caricato correttamente");
+let current = 0;
+const images = document.querySelectorAll('.carousel-img');
+
+function changeImage() {
+  images[current].classList.remove('active');
+  current = (current + 1) % images.length;
+  images[current].classList.add('active');
+}
+
+setInterval(changeImage, 3000); // Cambia immagine ogni 3 secondi
